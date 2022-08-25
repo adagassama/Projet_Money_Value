9071e605863ff68806fda0fbaa68c3667465ed5c
Adama <adamagassama40.ag@gmail.com>
2022-08-25 23:26:08 +0200

    Update header

--- Admin/src/components/Header.vue
+++ Admin/src/components/Header.vue
@@ -1,42 +1,67 @@
 <template>
   <nav class="navbar navbar-expand-custom navbar-mainbg">
-        <a class="navbar-brand navbar-logo" href="#">Navbar</a>
+		<img class="logo" :src="logo" />
+        <a class="navbar-brand navbar-logo" href="#">Money Value</a>
         <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fas fa-bars text-white"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
-                <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
                 <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                    <a class="nav-link" href="/admin"><i class="fas fa-tachometer-alt"></i>DASHBOARD</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-address-book"></i>Address Book</a>
+                    <a class="nav-link" href="/login"><i class="far fa-address-book"></i>LOGIN</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-clone"></i>Components</a>
+				<li class="nav-item active">
+                    <a class="nav-link" href="logout"><i class="far fa-address-book"></i>LOGOUT</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-calendar-alt"></i>Calendar</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-chart-bar"></i>Charts</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:void(0);"><i class="far fa-copy"></i>Documents</a>
-                </li>
+                
             </ul>
         </div>
     </nav>
 </template>
 
 <script>
-
+import logo from "../assets/logo.png"
+    export default {
+        data() {
+            return {
+				logo:logo,
+                employees: {},
+                currentUser: {},
+                token: localStorage.getItem('token')
+            }
+        },
+        methods: {
+            logout(){
+                axios.post('http://127.0.0.1:8000/api/logout').then((response) => {
+                    localStorage.removeItem('token')
+                    this.$router.push('/login')
+                }).catch((errors) => {
+                    console.log(errors)
+                })
+            }
+        },
+        mounted() {
+            window.axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`
+            axios.get('http://127.0.0.1:8000/api/user').then((response) => {
+                this.currentUser = response.data
+            }).catch((errors) => {
+                console.log(errors)
+            })
+        }
+    }
 </script>
 
 <style>
 body{
 	font-family: 'Roboto', sans-serif;
+}
+.logo{
+	width: 80px;
+	height: 80px;
+	margin-left: 150px;
 }
 * {
 	margin: 0;
@@ -51,7 +76,7 @@ i {
 	color: #fff;
 }
 .navbar-mainbg{
-	background-color: #5161ce;
+	background-color: #172b4d;
 	padding: 0px;
 }
 #navbarSupportedContent{
@@ -80,7 +105,7 @@ i {
     position: relative;
 }
 #navbarSupportedContent>ul>li.active>a{
-	color: #5161ce;
+	color: #ffffff;
 	background-color: transparent;
 	transition: all 0.7s;
 }
