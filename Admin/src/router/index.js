facccc11b8edc98c41a470e93dcf468245a8c5d6
Adama <adamagassama40.ag@gmail.com>
2022-08-25 23:26:34 +0200

    Add new route

--- Admin/src/router/index.js
+++ Admin/src/router/index.js
@@ -1,24 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
-
+import { createRouter, createWebHistory } from 'vue-router';
+import DashboardView from "../views/DashboardView.vue";
+import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import AddCurrencyView from "../views/AddCurrencyView.vue";
+import AddPairView from "../views/AddPairView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/admin',
       name: 'admin',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/DashboardView.vue')
+      component: DashboardView
     },
     {
       path: '/login',
       name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/LoginView.vue')
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/addCurrency',
+      name: 'addCurrency',
+      component: AddCurrencyView
+    },
+    {
+      path: '/addPair',
+      name: 'addPair',
+      component: AddPairView
     }
   ]
 })
