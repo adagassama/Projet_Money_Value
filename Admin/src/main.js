4fdb040f94e0ad87793c66fe4174105955c71630
Adama <adamagassama40.ag@gmail.com>
2022-08-26 22:12:32 +0200

    Save Token

--- Admin/src/main.js
+++ Admin/src/main.js
@@ -1,9 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Toaster from '@meforma/vue-toaster';
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
+
+function loggedIn(){
+    return localStorage.getItem('token')
+}
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(Toaster, {timeout: 3000})
 
 app.mount('#app')
