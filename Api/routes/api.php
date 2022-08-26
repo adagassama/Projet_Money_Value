6b156e2d4958851ca076baa1a05bd5e2e3aac197
Adama <adamagassama40.ag@gmail.com>
2022-08-26 22:13:33 +0200

    Update routes API

--- Api/routes/api.php
+++ Api/routes/api.php
@@ -20,8 +20,7 @@ use App\Http\Controllers\AuthController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('currencies', CurrencyController::class);
+Route::apiResource('currencies',CurrencyController::class);
 Route::apiResource('pairs', PairsController::class);
 
 Route::post('register', [AuthController::class,'register']);
